468652389b48219abe2d3704f1359f2a2aaa7efe
Danil Krutikov <kdanil.i@yandex.ru>
2024-03-05 20:43:16 +0700

    add webpack-server

---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const path = require('node:path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const MiniCSSExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: { 
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    compress: true,
+    port: 8080,
+  },
+  module: {   
+    rules: [  
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.html$/i,
+        use: ["html-loader"],
+      },
+      {
+        test: /\.css$/i,
+        use: [MiniCSSExtractPlugin.loader, 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      filename: 'main.html',
+    }),
+    new MiniCSSExtractPlugin(),
+  ]
+}
